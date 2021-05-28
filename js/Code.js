330a82cd313755e81f16028cb9f5bd8ee53ab95f
Sambhita <77112921+Sambhita@users.noreply.github.com>
2021-05-28 20:04:08 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "GENIE";
+const accessCode2 = "DALIA";
+const accessCode3 = "JASMINE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("N I E E G", 100,50)
+    fill("lightblue")
+    text("Hint: You ain't never had a friend like me", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("L A A I D", 640,150)
+    fill("lightblue")
+    text("Hint: She also serves as the Genie's love interest", 640,170)
+
+    fill("white")
+    textSize(15)
+    text("S M E J N I A", 100,250)
+    fill("lightblue")
+    text("Hint: You're not gonna find another girl like her in a million years", 100,270)
+
+}
